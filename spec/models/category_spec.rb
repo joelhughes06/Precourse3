b248662e3e2c2c58bbd1bc82420ee15daac204f1
Joel Hughes <joelhughes06@netscape.net>
2014-05-01 23:31:25 -0600

    050114

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -17,5 +17,46 @@ describe Category do
 #	end
 
 	it { should have_many(:titles)}
-	
+
 end
+
+	describe "#recent_videos" do
+		it "returns the videos in the reverse chronological order by created_at" do
+			comedies=Category.create(genre: "comedies")
+			futurama=Title.create(title: "Futurama", description: "Space Travel", iuy: "1", created_at: 1.day.ago)
+			south_park=Title.create(title: "South Park", description: "Crazy kids", category_id: "1", created_at: 2.days.ago)
+			expect(comedies.recent_videos).to eq([south_park, futurama])
+		end
+
+		it "returns all the videos if there are fewer than 6 videos" do
+			comedies=Category.create(genre: "comedies")
+			futurama=Title.create(title: "Futurama", description: "Space Travel", category_id: "1", created_at: 1.day.ago)
+			south_park=Title.create(title: "South Park", description: "Crazy kids", category_id: "1", created_at: 2.days.ago)
+			expect(comedies.recent_videos.count).to eq(2)
+		end
+
+		it "returns 6 videos if there are more than 6 videos" do
+			comedies=Category.create(genre: "comedies")
+			futurama=Title.create(title: "Futurama", description: "Space Travel", category_id: "1", created_at: 1.day.ago)
+			south_park=Title.create(title: "South Park", description: "Crazy kids", category_id: "1", created_at: 2.days.ago)
+			6.times {Title.create(title: "foo", description: "bar", category: comedies)}
+			tonights_show=Title.create(title: "Tonights show", description: "Talk show", category: comedies, created_at: 1.day.ago)
+			expect(comedies.recent_videos.count).to eq(6)
+		end
+
+		it "returns the most recent 6 videos" do
+			comedies=Category.create(genre: "comedies")
+			futurama=Title.create(title: "Futurama", description: "Space Travel", category_id: "3", created_at: 1.day.ago)
+			south_park=Title.create(title: "South Park", description: "Crazy kids", category_id: "3", created_at: 2.days.ago)
+			7.times {Title.create(title: "foo", description: "bar", category: comedies)}
+			tonights_show=Title.create(title: "Tonights show", description: "Talk show", category: comedies, created_at: 1.day.ago)
+			expect(comedies.recent_videos.count).not_to include(tonights_show)
+		end
+
+
+		it "returns an empty array if the category does not have any videos" do
+			comedies=Category.create(genre: "comedies")
+			expect(comedies.recent_videos).to eq([])
+		end
+	end
+	
